71fc06f9ecf3d07330a2349b23d15588c09c4bef
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-13 14:18:03 +0100

    Añadiendo metodo calcular diametro y introduciendo PI como propiedad de la clase por comodidad

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,11 +1,14 @@
 public class Circulo extends Punto {
     private double radio;
+    private final double PI = 3.14;
     public Circulo(double x, double y, double radio) {
         super(x,y);
         this.radio = radio;
     }
+    public double calcularDiametro() {
+        return radio*PI;
+    }
     public double calcularArea() {
-        final double PI = 3.14;
         return (radio*radio)*PI;
     }
 }
