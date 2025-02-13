b39aedff3c9c5868cdcf0c320d97136b43f9670b
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-13 13:54:57 +0100

    Declarando ejemplos

--- src/Herencia/App.java
+++ src/Herencia/App.java
@@ -1,4 +1,10 @@
 package Herencia;
 
 public class App {
+    public static void main(String[] args) {
+        Persoa persoa1 = new Persoa("Juan","1314214G");
+        Deportista deportista1 = new Deportista(persoa1,"Club de los aMIGOs",23);
+        Deportista deportista2 = new Deportista("Jlkje","alksfj","MLKS","lafj",3);
+        System.out.println(deportista1.getClub());
+    }
 }
