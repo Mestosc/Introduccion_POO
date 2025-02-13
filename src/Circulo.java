3ecf2e2ed5ba436fd994257ad4cda79e091ca202
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-13 14:15:19 +0100

    Añadiendo metodo para calcular el area

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,6 +1,11 @@
 public class Circulo extends Punto {
     private double radio;
-    public Circulo(double x, double y, double z) {
+    public Circulo(double x, double y, double radio) {
         super(x,y);
+        this.radio = radio;
+    }
+    public double calcularArea() {
+        final double PI = 3.14;
+        return (radio*radio)*PI;
     }
 }
