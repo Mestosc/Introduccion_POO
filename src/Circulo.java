01925f906d40588a50834d30b34a890649dfb6b5
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-13 14:19:20 +0100

    Añadiendo getters y setters

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,6 +1,19 @@
 public class Circulo extends Punto {
     private double radio;
     private final double PI = 3.14;
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getPI() {
+        return PI;
+    }
+
     public Circulo(double x, double y, double radio) {
         super(x,y);
         this.radio = radio;
