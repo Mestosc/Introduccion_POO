365816aacb34d65a0d7bc1bcc3f59b5586ce4d55
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-17 14:42:46 +0100

    Añadiendo cosas

--- src/Prisma.java
+++ src/Prisma.java
@@ -1,6 +1,14 @@
-public class Prisma {
-    private double base;
-    private double profundidad;
+public class Prisma extends Cuadrado {
     private double altura;
 
+    public Prisma(double lado, double altura) {
+        super(lado);
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        double areaBase = 2*Math.pow(lado,2);
+        double areaLateral = 4 * lado * altura;
+        return areaBase + areaLateral;
+    }
 }
