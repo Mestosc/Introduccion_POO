7992c61e7cb4a39229019711f87df3346447db15
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-17 13:16:27 +0100

    Cilindro creado y configurado creo

---
+++ src/Herencia/Cilindro.java
@@ -0,0 +1,26 @@
+package Herencia;
+
+public class Cilindro extends Circulo {
+    private double altura;
+    public Cilindro(Centro centro, double radio, double altura) {
+        super(centro, radio);
+        this.altura = altura;
+    }
+
+    public Cilindro(double x, double y, double radio) {
+        super(x, y, radio);
+        this.altura = radio;
+    }
+
+    public Cilindro(int x, int y, int radio) {
+        super(x, y, radio);
+    }
+    public double calcularArea() {
+        double areaBases = super.calcularArea()*2;
+        double areaLateral = 2 * Math.PI * altura;
+        return areaBases + areaLateral;
+    }
+    public double calcularVolumen() {
+        return Math.PI * Math.pow(getRadio(),2) * altura;
+    }
+}
