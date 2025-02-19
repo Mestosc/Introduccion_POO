3ac3a54e8f94f006cf20b501511861f0767662e4
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-19 09:02:04 +0100

    Cambios varios

--- src/Herencia/App.java
+++ src/Herencia/App.java
@@ -2,13 +2,6 @@ package Herencia;
 
 public class App {
     public static void main(String[] args) {
-        Circulo circulo = new Circulo(2,10,5);
-        Cilindro cilindro1 = new Cilindro(12,10,5);
-        Cilindro cilindro2 = new Cilindro(circulo);
-        System.out.println(cilindro1.calcularVolumen());
-        System.out.println(cilindro1.calcularArea());
-        System.out.println(cilindro1.calcularDiametro());
-        System.out.println(circulo);
-        System.out.println(cilindro2);
+
     }
 }
