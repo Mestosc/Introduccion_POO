92820df60690c36a4a7b53c33b491768ef88b9dd
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-19 09:18:32 +0100

    Añadiendo cosas

--- src/Herencia/App.java
+++ src/Herencia/App.java
@@ -2,6 +2,7 @@ package Herencia;
 
 public class App {
     public static void main(String[] args) {
-
+        Cilindro cilindro1 = new Cilindro(12,10,10,10);
+        System.out.println(cilindro1.calcularArea());
     }
 }
