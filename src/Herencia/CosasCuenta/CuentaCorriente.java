096fbaebdbbee8f68075bffd7516c3d182bb3c32
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-19 09:25:47 +0100

    Añadiendo cosas

--- src/Herencia/CosasCuenta/CuentaCorriente.java
+++ src/Herencia/CosasCuenta/CuentaCorriente.java
@@ -1,9 +1,52 @@
 package Herencia.CosasCuenta;
 
 public class CuentaCorriente {
-    double saldo;
-    String titular;
-    double numeroCuota;
-    String nif;
+    private double saldo;
+    private String titular;
+    private double numeroCuota;
+    private String nif;
 
+    public CuentaCorriente(double saldo, String nif, double numeroCuota, String titular) {
+        this.saldo = saldo;
+        this.nif = nif;
+        this.numeroCuota = numeroCuota;
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public double getNumeroCuota() {
+        return numeroCuota;
+    }
+
+    public void setNumeroCuota(double numeroCuota) {
+        this.numeroCuota = numeroCuota;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double sacarDinero(double dineroSacar) {
+        saldo -= dineroSacar;
+        return saldo;
+    }
 }
---
+++ src/Herencia/CosasCuenta/CuentaCorrienteTest.java
@@ -0,0 +1,11 @@
+package Herencia.CosasCuenta;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaCorrienteTest {
+    CuentaCorriente = ne
+    @org.junit.jupiter.api.Test
+    void sacarDinero() {
+
+    }
+}
