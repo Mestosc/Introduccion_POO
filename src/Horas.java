7d6d553d196916ae9aeb49b1ed004d588d5e0529
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-20 12:42:25 +0100

    Añadiendo constructor

--- src/Horas.java
+++ src/Horas.java
@@ -3,6 +3,12 @@ public class Horas {
     private int minutos;
     private int segundos;
 
+    public Horas(int horas, int minutos, int segundos) {
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
     public int getHoras() {
         return horas;
     }
