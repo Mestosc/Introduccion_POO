0050ca5fe5e6ffb954f0fb1d438542c7652ac021
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-20 12:41:06 +0100

    Añadiendo cosas al Main

--- src/Herencia/CosasCuenta/Main.java
+++ src/Herencia/CosasCuenta/Main.java
@@ -2,7 +2,7 @@ package Herencia.CosasCuenta;
 
 public class Main {
     public static void main(String[] args) {
-        CuentaCorriente cuentaCorriente = new CuentaCorriente(1021,"120120","afasfasf","LKJ");
+        CuentaCorriente cuentaCorriente = new CuentaCorriente(1021,"12012039J","41234L23G","LKJ");
         System.out.println(cuentaCorriente.getNif());
     }
 }
