ad42e055a668f85e3d842e25d24a49730a867fb4
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-20 12:41:25 +0100

    Añadiendo getters y setters

--- src/Horas.java
+++ src/Horas.java
@@ -3,4 +3,27 @@ public class Horas {
     private int minutos;
     private int segundos;
 
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
 }
