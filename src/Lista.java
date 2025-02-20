d33db94fd4a306178722f34568ef8f0135644e4c
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-20 14:36:00 +0100

    Añadiendo firstAppend

--- src/Lista.java
+++ src/Lista.java
@@ -5,6 +5,14 @@ public class Lista {
     public Lista() {
         numeros = new int[0];
     }
+    public void firstAppend(int numero) {
+        numeros = Arrays.copyOf(numeros,numeros.length+1);
+        int[] new_numeros = Arrays.copyOfRange(numeros,0, numeros.length-1);
+        for (int i = 1, j = 0; j < new_numeros.length; i++,j++) {
+            numeros[i] = new_numeros[j];
+        }
+        numeros[0] = numero;
+    }
     public void append(int numero) {
         numeros = Arrays.copyOf(numeros,numeros.length+1);
         numeros[numeros.length-1] = numero;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Lista lista1 = new Lista();
-        lista1.append(2);
+        lista1.firstAppend(10);
         for (int i : lista1.numeros) {
             System.out.println(i);
         }
