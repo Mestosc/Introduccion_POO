86fd9c45441892cf7fa779952caa1710c00875a5
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-24 13:33:44 +0100

    Añadiendo Nodo y ListaNodos

---
+++ src/ListaNodos.java
@@ -0,0 +1,4 @@
+public class ListaNodos {
+    Nodo[] nodos;
+
+}
---
+++ src/Nodo.java
@@ -0,0 +1,16 @@
+public class Nodo {
+    private int valor;
+    private Nodo punteiroSeguinte;
+    public int getValor() {
+        return valor;
+    }
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    public Nodo getSeguinteNodo() {
+        return punteiroSeguinte;
+    }
+    public void setPunteiroSeguinte(Nodo punteiroSeguinte) {
+        this.punteiroSeguinte = punteiroSeguinte;
+    }
+}
