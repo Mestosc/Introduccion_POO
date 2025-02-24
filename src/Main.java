d71dde78c6f1de45ad30996c4891265672b3688a
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-24 17:59:40 +0100

    Pruebas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         ListaNodos nodos = new ListaNodos();
-        ListaNodos nodo = new ListaNodos(1,23,4,5);
+        ListaNodos nodo = new ListaNodos(1,23,4,5,10,20);
+        nodo.engadirUltimo(40);
+        nodo.engadirPrimero(0);
+        nodo.listar();
     }
 }
