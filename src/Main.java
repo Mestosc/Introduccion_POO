82efe7fb81cad259b4adb73e43b253f907843b6e
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-24 12:51:22 +0100

    Haciendo cambios

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Lista lista2 = new Lista(1,2,3,5,6,7,8,9);
         lista2.insert(8,20);
+        lista2.deleteFirst();
         for (int i : lista2.getNumeros()) {
             System.out.println(i);
         }
         System.out.println("-----LISTA 2----");
         Lista lista = new Lista();
-        lista.append(20);
-        lista.append(10);
-        lista.append(100);
-        lista.insert(1,500);
         for (int i = 0; i < lista.getNumeros().length; i++) {
             System.out.println(lista.getNumeros()[i]);
         }
