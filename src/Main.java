989c95566e458d74cda5c8654984689b3c967f8a
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-24 19:31:19 +0100

    Haciendo cosas

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         ListaNodos nodos = new ListaNodos();
-        ListaNodos nodo = new ListaNodos(1,2,4,5,10,20);
-        nodo.deleteElement(3);
+        ListaNodos nodo = new ListaNodos(1,2,4);
         nodo.listar();
+        System.out.println(nodo.getTamano());
     }
 }
