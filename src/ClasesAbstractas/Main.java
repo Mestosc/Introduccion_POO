5c668de1cd585914698c7ab15b0b566ef51e1374
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-26 10:27:04 +0100

    Cambios al Main

--- src/ClasesAbstractas/Main.java
+++ src/ClasesAbstractas/Main.java
@@ -5,6 +5,6 @@ public class Main {
         Cuadrado cuadrado1 = new Cuadrado(4,5,-10);
         System.out.println(cuadrado1.aCadea());
         Cilindro cilindro = new Cilindro(4,5,4,5);
-        System.out.println(cilindro.calcularVolume());
+        System.out.println(cilindro.aCadea());
     }
 }
