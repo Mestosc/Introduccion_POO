bb4e98f27b1fde13f2d0ddb1acda35c67599f4a6
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-26 09:45:29 +0100

    Añadiendo cosas

--- src/ClasesAbstractas/FiguraXeometrica.java
+++ src/ClasesAbstractas/FiguraXeometrica.java
@@ -2,4 +2,8 @@ package ClasesAbstractas;
 
 abstract public class FiguraXeometrica extends PuntoB {
     abstract public double calcularArea();
+    public FiguraXeometrica(double x, double y) {
+        super(x,y);
+    }
+
 }
--- src/ClasesAbstractas/PuntoB.java
+++ src/ClasesAbstractas/PuntoB.java
@@ -4,6 +4,10 @@ abstract public class PuntoB {
     private double x;
     private double y;
     public PuntoB() {}
+    public PuntoB(double x, double y) {
+        this.x= x;
+        this.y = y;
+    }
     public double getX() {
         return x;
     }
