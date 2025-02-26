853d7736d0b03fdeb2de420c630aa6c91ffe5f28
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-26 09:47:26 +0100

    Haciendo cambios

--- src/ClasesAbstractas/Main.java
+++ src/ClasesAbstractas/Main.java
@@ -3,5 +3,6 @@ package ClasesAbstractas;
 public class Main {
     public static void main(String[] args) {
         Cuadrado cuadrado1 = new Cuadrado(4,5,3);
+        System.out.println(cuadrado1.aCadea());
     }
 }
