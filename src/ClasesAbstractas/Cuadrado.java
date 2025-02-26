529b5cc63de1b93b355a8a38748c2c213cf5571c
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-26 09:29:07 +0100

    Haciendo pruebas para ver el uso de una clase Abstracta

---
+++ src/ClasesAbstractas/Cuadrado.java
@@ -0,0 +1,22 @@
+package ClasesAbstractas;
+
+public class Cuadrado extends FiguraXeometrica{
+    int lado;
+    public Cuadrado(double x, double y, int lado) {
+        this(lado);
+        setX(x);
+        setY(y);
+    }
+    public Cuadrado(int lado) {
+        this.lado = lado;
+    }
+    @Override
+    public double calcularArea() {
+        return lado*lado;
+    }
+
+    @Override
+    void aCadea() {
+        System.out.println("X:" + getX() + " Y:" + getY() + " Area:" + calcularArea());
+    }
+}
