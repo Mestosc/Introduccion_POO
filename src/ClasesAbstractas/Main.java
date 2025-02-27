45b704d685cfcaef43c9808686c8b99832dae1cb
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-27 12:55:18 +0100

    Haciendo cosas en Main para hacer la prueba

--- src/ClasesAbstractas/Main.java
+++ src/ClasesAbstractas/Main.java
@@ -2,6 +2,9 @@ package ClasesAbstractas;
 
 public class Main {
     public static void main(String[] args) {
-
+        Serpiente serpiente1 = new Serpiente("Juana",20);
+        serpiente1.comer();
+        Perro perro1 = new Perro("Amigo",2);
+        perro1.comer();
     }
 }
