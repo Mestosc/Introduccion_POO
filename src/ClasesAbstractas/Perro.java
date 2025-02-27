0e2632fe661f6adc822a14b09606e37ba80af669
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-27 13:36:20 +0100

    Haciendo pruebas

--- src/ClasesAbstractas/Perro.java
+++ src/ClasesAbstractas/Perro.java
@@ -4,8 +4,15 @@ public class Perro extends Mamiferos {
     public Perro(String nombre, int edad) {
         super(nombre,edad);
     }
+
+    @Override
+    public void parir() {
+        System.out.println("El perro pare");
+    }
+
     @Override
     void desprazar() {
         System.out.println("Se desplaza a cuatro patas");
     }
+
 }
