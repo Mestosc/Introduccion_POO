9d03728304fdf72ac2a1ed19cef4036e8cb581af
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-03-14 09:20:03 +0100

    Subiendo cosas

--- tests/ListaTest.java
+++ tests/ListaTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListaTest {
-    Lista lista1 = new Lista(1,2,3,5,6,7);
 
     @ParameterizedTest(name = "{0} into lista1 in index {1} is {0}")
     @CsvSource({
@@ -14,9 +13,11 @@ class ListaTest {
                     "-1,0,-1",
                     "8,10,8",
                     "-1,-1,-1",
-                    "2,1,2"
+                    "2,1,2",
+                    "100,1000,100"
     })
     void insert(int numInsert, int index, int expected) {
+        Lista lista1 = new Lista(1,2,3,5,6,7);
         assertEquals(expected,lista1.insert(index,numInsert), () -> "In " + index + " index " + "must be this number:" + numInsert);
     }
 }
