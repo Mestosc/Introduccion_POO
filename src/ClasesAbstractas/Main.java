53e54e9dec254f96afe390c8e783d1166971a4d0
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-03-19 08:32:47 +0100

    Haciendo cosas para probar

--- src/ClasesAbstractas/Main.java
+++ src/ClasesAbstractas/Main.java
@@ -4,9 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Serpiente serpiente1 = new Serpiente("Juana",20);
         serpiente1.comer();
-        Perro perro1 = new Perro("Amigo",2);
-        perro1.comer();
-        perro1.mamar();
-        perro1.voz();
+        Animal perro = new Perro("jasldf",3);
+        animalhacer(serpiente1);
+    }
+    static void animalhacer(Animal animal) {
+        if (animal instanceof Mamiferos anim) {
+            anim.parir();
+        }
+        else {
+            animal.comer();
+        }
     }
 }
