c4126a669e34c62b27c6be24cb5cd3f170a18d50
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-04-23 09:31:28 +0200

    Haciendo pruebas

--- src/Nodos/Main.java
+++ src/Nodos/Main.java
@@ -3,6 +3,9 @@ package Nodos;
 public class Main {
     public static void main(String[] args) {
         ListaNodos<Integer> nodo = new ListaNodos<>(1,2,4,10,20,200);
+        nodo.append(300);
+        ListaNodos<String> cadenas = new ListaNodos<>("Hola","individuo","eres","tu","quien","crees");
+        System.out.println(cadenas.get(2));
     }
 }
 
